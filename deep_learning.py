078caf58092ff16db0f859cc202543472531acfc
ericlee <lee3388@purdue.edu>
2021-11-28 21:36:59 -0500

    :sparkles: Add LSTM model

--- deep_learning.py
+++ deep_learning.py
@@ -1,37 +1,13 @@
 import pandas as pd
-df = pd.read_csv('./assets/preprocessed.csv', encoding='ISO-8859-1')
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import LinearSVC
-#from xgboost import XGBClassifier
-import numpy as np
 from tensorflow import keras
 from tensorflow.keras import layers
-
-classifiers = (
-    # LogisticRegression(max_iter=100000),
-    # KNeighborsClassifier(n_neighbors=10),
-    # GaussianNB(),
-    # RandomForestClassifier(max_depth=20, n_estimators=1000),
-    LinearSVC(max_iter=100000),
-    # XGBClassifier(use_label_encoder=False, eval_metric='mlogloss')
-)
-
-from embed_bag_of_words import load_embedded_bag_of_words
-from embed_tf_idf import load_embedded_tf_idf
 from embed_word2vec import load_embedded_word2vec
-from embed_glove import load_embedded_glove
 
-embeddings = {#'bag of words': load_embedded_bag_of_words,
-              #'tf idf': load_embedded_tf_idf,
-              'word2vec': load_embedded_word2vec,
-              'glove': load_embedded_glove}
+df = pd.read_csv('./assets/preprocessed.csv', encoding='ISO-8859-1')
+
+embeddings = {'word2vec': load_embedded_word2vec}
 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-import time
 
 labels = df['label'].to_numpy()
 for name, load_func in embeddings.items():
@@ -40,34 +16,29 @@ for name, load_func in embeddings.items():
     X_train, X_test, y_train, y_test = train_test_split(data, labels,
                                                         test_size=0.2,
                                                         random_state=42)
-    max_features = 20000
+    # max length of word2vec we have now
+    max_features = 300
     # Input for variable-length sequences of integers
+
     inputs = keras.Input(shape=(None,), dtype="int32")
     # Embed each integer in a 128-dimensional vector
+    x = keras.Sequential()
     x = layers.Embedding(max_features, 128)(inputs)
     # Add 2 bidirectional LSTMs
-
-    x = layers.Bidirectional(layers.LSTM(64, return_sequences=True))(x)
+    x = layers.Bidirectional(layers.LSTM(128, return_sequences=True))(x)
     x = layers.GlobalMaxPool1D()(x)
-    #x = layers.Bidirectional(layers.LSTM(16))(x)
-    x = layers.Dropout(0.4)(x)
-    x = layers.Dense(32, activation='relu')(x)
-    x = layers.Dropout(0.4)(x)
+    x = layers.Dropout(0.5)(x)
+    x = layers.Dense(64, activation='relu')(x)
+    x = layers.Dropout(0.5)(x)
+    x = layers.Dense(16, activation='relu')(x)
+    x = layers.Dropout(0.5)(x)
     # Add a classifier
-    outputs = layers.Dense(3, activation="relu")(x)
+    outputs = layers.Dense(3, activation="softmax")(x)
     model = keras.Model(inputs, outputs)
     model.summary()
-    model.compile("adam", "mse", metrics=["accuracy"])
-    model.fit(X_train, y_train, batch_size=32, epochs=2, validation_data=(X_test, y_test))
-
-    '''
-    for classifier in classifiers:
-        classifier.n_jobs = -1
-        start_time = time.time()
-        classifier.fit(X_train, y_train)
-        time_elapsed = time.time() - start_time
-        y_pred = classifier.predict(X_test)
-        acc_test = accuracy_score(y_test, y_pred)
-        print(f'{classifier.__class__.__name__} test accuracy: {acc_test:.3f}, training time: {time_elapsed}')
-    print()
-    '''
+    model.compile(optimizer="adam", loss='sparse_categorical_crossentropy', metrics=["accuracy"])
+    history = model.fit(X_train, y_train, batch_size=16, epochs=1, validation_split=0.2)
+    loss, acc = model.evaluate(x=X_test,
+                               y=y_test)
+    print("test loss: ", loss, ", test acc: ", 100 * acc, "%")
+    model.save('lstm_model')
---
+++ deep_learning_origin_data.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from sklearn import model_selection
+from sklearn.preprocessing import LabelEncoder
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras import preprocessing
+import matplotlib.pyplot as plt
+
+EPOCHS = 16
+BATCH_SIZE=16
+MAX_SEQUENCE_LENGTH = 266
+
+PREPROCESSED_DATA_PATH = 'assets/preprocessed.csv'
+df = pd.read_csv(PREPROCESSED_DATA_PATH, encoding='ISO-8859-1')
+data = df['text']
+data.fillna('', inplace=True)
+MAX_NB_WORDS = 50000
+tokenizer = preprocessing.text.Tokenizer(num_words=MAX_NB_WORDS, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~', lower=True)
+data = tokenizer.texts_to_sequences(data.values)
+data = preprocessing.sequence.pad_sequences(data, maxlen=MAX_SEQUENCE_LENGTH)
+labels = df['label'].to_numpy()
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(
+    data, labels, test_size=0.2, random_state=42)
+
+y_train = tf.keras.utils.to_categorical(LabelEncoder().fit_transform(y_train))
+y_test = tf.keras.utils.to_categorical(LabelEncoder().fit_transform(y_test))
+
+# build LSTM model
+# Text Input
+text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
+embedding_vector_feature = 64
+voc_size = 20000
+model = keras.Sequential()
+model.add(layers.Embedding(voc_size, embedding_vector_feature, input_length=MAX_SEQUENCE_LENGTH))
+model.add(layers.SpatialDropout1D(0.2))
+# model.add(layers.Embedding(voc_size, embedding_vector_feature)(text_input))
+model.add(layers.Bidirectional(layers.LSTM(32, return_sequences=True)))
+model.add(layers.Dropout(0.4))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dropout(0.4))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dropout(0.4))
+model.add(layers.Dense(3, activation='softmax'))
+model.compile(loss='sparse_categorical_crossentropy',optimizer='adam', metrics=['accuracy'])
+print(model.summary())
+
+# Train
+history = model.fit(x=X_train,
+                    y=y_train,
+                    validation_split=0.2,
+                    epochs=EPOCHS,
+                    verbose=1,
+                    batch_size=BATCH_SIZE)
+# Evaluation
+loss, acc = model.evaluate(x=X_test,
+                           y=y_test)
+print("test loss: ", loss, ", test acc: ", 100 * acc, "%")
+model.save('lstm_original_data')
